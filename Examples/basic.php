8f4d840c840517224a0a915ea28780d8e8114c34
irismanriquezc <imanriquez@csumb.edu>
2019-02-27 04:46:07 +0000

    done with lab 4

---
+++ Examples/basic.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> </title>
+    </head>
+    <body>
+   
+    <?php 
+    
+    $weekdays = array();
+    $weekdays[] = "M";
+    $weekdays[] = "T"; 
+    array_push($weekdays,"W"); 
+    echo "Displaying values using var_dump: ";
+    var_dump($weekdays);
+    echo "<br><br>";
+    echo "Displaying values using print_r: ";
+    print_r($weekdays);
+
+//     $n=0;
+//     for ($i=0; $i<9; $i++) {
+//     $random= rand(0,1000);
+//     echo $random . " ";
+//     if($random%2==0){
+//         echo "Even". "<br><br>";
+//     }
+//     else{
+//         echo "Odd". "<br><br>";
+//     }
+// 	  $n+=$random;  //displays values from 0 to 5
+	   
+//               }
+//     $average= $n/9;
+//     echo "Sum: ". $n  . "<br><br>";
+   
+//     echo "Average: ".$average . "<br><br>";
+
+    // echo $n  . "<br><br>";
+    
+    // $n = rand(5,15);   
+    // echo $n  . "<br><br>";
+    
+    // $n = "hElLo WoRlD!";
+    // echo strtoupper($n)  .  "<br><br>";
+?>
+ <table>
+        
+    </table>
+    </body>
+</html>
